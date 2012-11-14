55d28bf04be0f7e58f1a4d518737871a9d5f7b83
daedalusrex <daedalusrex@gmail.com>
2012-11-14 03:44:54 -0500

    Added some structure stuff to tile class, does not compile

--- zenilib/jni/application/Winter War Files/Tile.cpp
+++ zenilib/jni/application/Winter War Files/Tile.cpp
@@ -1,11 +1,21 @@
 #include "Tile.h"
+#include "Structure.h"
 
-
-Tile::Tile(void)
+Tile::Tile(void)	:
+	covering(HARD_SNOW),
+	Building(0)
 {
+	
+
 }
 
 
 Tile::~Tile(void)
 {
+
 }
+
+bool Tile::has_building()	{
+	return !(Building == 0);
+}
+
--- zenilib/jni/application/Winter War Files/Tile.h
+++ zenilib/jni/application/Winter War Files/Tile.h
@@ -1,10 +1,27 @@
 #pragma once
 #include "Renderable.h"
+
+enum TILE_TYPE	{
+	SOFT_SNOW, HARD_SNOW, ICE
+};
+
+class Structure;
+
 class Tile :
 	public Renderable
 {
+	TILE_TYPE	covering;
+	Structure* Building;
+
 public:
+
+	bool has_building();
+
 	Tile(void);
 	~Tile(void);
+
+
+
+
 };
 
