edfa41afdd80aa5a71e558c0979612a74a1ec56e
Daniel Scalise <dlees@caen-vnc08.engin.umich.edu>
2012-11-17 15:01:54 -0500

    4 player Splitscreen! One Resolution

--- zenilib/jni/application/Winter War Files/bootstrap.cpp
+++ zenilib/jni/application/Winter War Files/bootstrap.cpp
@@ -31,12 +31,22 @@ public:
 	, m_next(false)
 	, escape(false)
 	, m_prev_clear_color(get_Video().get_clear_Color())
-	, Perm(Point3f(10,10,0), Vector3f(1,1,1))
+	, Perm(Point3f(30,10,0), Vector3f(2,2,2))
+	,Rend(Point3f(1,0,20))
+	,Rend2(Point3f(10,30,100))
+	,Rend3(Point3f(0,0,50))
+	,Rend4(Point3f(10,10,20))
 
   {
     set_pausable(true);
 	view.add_renderable(&Rend);
 	view.add_player_view(new Player_View(&Rend));
+	view.add_renderable(&Rend2);
+	view.add_player_view(new Player_View(&Rend2));
+	view.add_renderable(&Rend3);
+	view.add_player_view(new Player_View(&Rend3));
+	view.add_renderable(&Rend4);
+	view.add_player_view(new Player_View(&Rend4));
 	view.add_renderable(&Perm);
   }
 
@@ -45,6 +55,9 @@ private:
 
   bool escape;
   Player Rend;
+  Player Rend2;
+  Player Rend3;
+  Player Rend4;
   Permanent Perm;
   View view;
 
@@ -96,7 +109,16 @@ private:
 	 
   void on_mouse_motion(const SDL_MouseMotionEvent &event) {
     Rend.adjust_pitch(event.yrel / 500.0f);
-    Rend.turn_left(-event.xrel / 500.0f);
+    Rend.turn_left(-event.xrel / 500.0f);    
+
+	Rend2.adjust_pitch(event.yrel / 500.0f);
+    Rend2.turn_left(-event.xrel / 500.0f);   
+
+	Rend3.adjust_pitch(event.yrel / 500.0f);
+    Rend3.turn_left(-event.xrel / 500.0f);  
+
+	Rend4.adjust_pitch(event.yrel / 500.0f);
+    Rend4.turn_left(-event.xrel / 500.0f);
   }
 
 
