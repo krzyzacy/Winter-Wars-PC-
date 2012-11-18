9dddb212cef50e03dd52fcaebefe2518e11a7f02
krzyzacy <lusensjtu@gmail.com>
2012-11-18 18:10:47 -0500

    Revert "Revert "Revert "delete .gitignore"""
    This reverts commit ad87006a332eca2632edf33bc93a39421737f839.

--- zenilib/jni/application/Winter War Files/Tile.h
+++ zenilib/jni/application/Winter War Files/Tile.h
@@ -22,7 +22,7 @@ public:
 	virtual const model_key_t get_model_name() const;
 
 	Tile(const Zeni::Point3f &center__ = Zeni::Point3f(0.0f,0.0f,0.0f),
-		 const Zeni::Vector3f &scale__ = Zeni::Vector3f(10.0f,10.0f,10.0f) 
+		 const Zeni::Vector3f &scale__ = Zeni::Vector3f(1.0f,1.0f,1.0f) 
 		);
 	~Tile(void);
 
