817a1be142fa1f54a56237225bbd341ebe4f6a01
dlees <dlees@umich.edu>
2012-11-21 15:20:58 -0500

    Update zenilib/jni/application/Winter War Files/Player_View.cpp

--- zenilib/jni/application/Winter War Files/Player_View.cpp
+++ zenilib/jni/application/Winter War Files/Player_View.cpp
@@ -20,3 +20,9 @@ void Player_View::set_camera(const Point2f &topLeft, const Point2f &bottomRight)
 {
 	get_Video().set_3d_view(player->get_camera(), std::make_pair(topLeft, bottomRight));
 }
+
+void Player_View::render_hud()
+{
+
+
+}
