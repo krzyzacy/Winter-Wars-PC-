4042edcda3546c6eedbda685b096a99ff3415671
daedalusrex <daedalusrex@gmail.com>
2012-11-25 16:59:02 -0500

    Added some network functions to team

--- zenilib/jni/application/Winter War Files/Team.cpp
+++ zenilib/jni/application/Winter War Files/Team.cpp
@@ -1,4 +1,10 @@
 #include "Team.h"
+#include "World.h"
+#include "Player.h"
+
+
+using namespace std;
+using namespace Zeni;
 
 
 Team::Team(void)
--- zenilib/jni/application/Winter War Files/Team.h
+++ zenilib/jni/application/Winter War Files/Team.h
@@ -1,23 +1,48 @@
 #pragma once
+
+#include <zenilib.h>
+
+class World;
+class Tile;
+class Player;
+class Base;
+
 class Team
-{
+{	
+public:
+	Team(void); //Enter Players, And Color?
+	~Team(void);
+
+	
+	bool Is_Tree_Claimed();
+	void Start_Victory_Countdown();
+	bool Hail_to_the_Victors();	//Tells the rest of the game we've won
+
+	void update(float time);
+	void check_connectivity();	//Deactivates tiles if they aren't connected
+	void add_tile(Tile *);
+	void remove_tile(Tile *);
+
 
 private:
 	//list of players
+	std::list<Player*>	members;
 	//Resource value
-	//Class network 
+	float Ice_Blocks;
+	float intake_rate;
+
+	//Class network (new Class)
+	World*	Land;
+
+	std::set<Tile*>	Newtwork;
+	std::set<Tile*> Adjacent_Tiles;
 		//Holds tile information pertininet to team
+	std::set<Tile*> Disconnected_Tiles;
 
-	//Check connectivity (sees if stuff is there)
-
-	//Victory Condition check
-	//Base??
+	Base* Home;
 
 
 
-public:
-	Team(void);
-	~Team(void);
 
 };
 
