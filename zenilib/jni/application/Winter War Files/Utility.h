02678054206e42a854d04d368ca33a26a80c0b10
Sen Lu <lusensjtu@gmail.com>
2013-04-26 15:47:45 -0300

    add self_addr to Player_info

--- zenilib/jni/application/Winter War Files/Utility.h
+++ zenilib/jni/application/Winter War Files/Utility.h
@@ -6,6 +6,8 @@
 
 #include <zenilib.h>
 
+#include "RakPeerInterface.h"
+
 #define MAX_CLIENTS 10
 #define SERVER_PORT 60000
 #define ROOM_CAPACITY 16
@@ -17,5 +19,6 @@ struct Player_info{
 	Zeni::String genders_;
 	int controls_;
 	int sensitivities_;
+	RakNet::SystemAddress self_addr;
 };
 
