28e1474ea6e836de80f8faeca69de8937403d1ec
dlees <dlees@umich.edu>
2013-05-08 13:50:11 -0400

    added Death_Match to solution

--- zenilib/jni/application/Winter War Files/Tutorial_Level.cpp
+++ zenilib/jni/application/Winter War Files/Tutorial_Level.cpp
@@ -11,7 +11,7 @@ Tutorial_Level::Tutorial_Level()
 {
 	current_objective = new Build_Structure(HEALING_POOL);
 	
-	-- -- -- -- -- -- --parameters.at(4);
+	parameters.find("Snow Factory Intake per Second").get_value();
 
 }
 
