466ab25c3d574893dc669272fc54ac8dbf9448e3
Sen Lu <lusensjtu@gmail.com>
2013-05-09 20:36:33 -0400

    update moveable logic

--- zenilib/jni/application/Winter War Files/Moveable.cpp
+++ zenilib/jni/application/Winter War Files/Moveable.cpp
@@ -36,12 +36,39 @@ void Moveable::update(const float &time)	{
 
 	center += velocity * time;
 
+	Vector3f player_pos(center);
+
 	if (Game_Model::get().get_World()->get_tile(center) == NULL){
 		off_map();
 	}
-	else if (Game_Model::get().get_World()->get_ground_height(center) 
+	/*else if (Game_Model::get().get_World()->get_ground_height(center) 
 				>= get_bottom_center().z + 10.0f){
 		hit_tile();
+	}*/
+	else{
+
+		for(double i = -100.0; i <= 100.0; i = i + 10){
+			Vector3f pos_dir(i / 100, sqrt(1 - (i / 100) * (i / 100)), 0);
+			Vector3f neg_dir(i / 100, - sqrt(1 - (i / 100) * (i / 100)), 0);
+
+			if(Game_Model::get().get_World()->get_tile(Point3f(player_pos + pos_dir * size.x / 2 )) == NULL){
+				off_map();
+				return;
+			}
+			else if(Game_Model::get().get_World()->get_ground_height(Point3f(player_pos + pos_dir * size.x / 2 )) >= get_bottom_center().z + 10.0f){
+				hit_tile());
+				return;
+			}
+
+			if(Game_Model::get().get_World()->get_tile(Point3f(player_pos + neg_dir * size.x / 2 )) == NULL){
+				off_map();
+				return;
+			}
+			else if(Game_Model::get().get_World()->get_ground_height(Point3f(player_pos + neg_dir * size.x / 2 )) >= get_bottom_center().z + 10.0f){
+				hit_tile();
+				return;
+			}
+		}
 	}
 }
 
